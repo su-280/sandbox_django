19a24bf76765293a9f02162eaece58424e45fbdc
student <student@o-hara-kashiwa.local>
2020-09-24 15:14:51 +0900

    mail送信まで

--- diary/forms.py
+++ diary/forms.py
@@ -1,4 +1,22 @@
 from django import forms
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
-    pass
+    name = forms.CharField(label='お名前', max_length=30)
+    email = forms.EmailField(label='メールアドレス')
+    title = forms.CharField(label='タイトル', max_length=30)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control col-9'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前を入力してください'
+
+    def send_email(self):
+        subject = 'hello'
+        mail_to = ['a@abc.jp',]
+        mail_from = 'b@abc.jp'
+        body = 'テストメール'
+        mail_msg = EmailMessage(subject=subject, from_email=mail_from, to=mail_to, body=body)
+        mail_msg.send()
+        
